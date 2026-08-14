0f29657f4c6e22202d561bd72729a6a499d424d5
Aashka Panta <aashikapanta1973@gmail.com>
2026-08-14 17:29:56 +0545

    selector js update

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
+import { selector } from "../support/selector";
 
 const email = faker.internet.email();
+
 describe("Registration functionality", () => {
   const username = `aashika_${Date.now().toString().slice(-4)}`;
 
@@ -10,170 +12,179 @@ describe("Registration functionality", () => {
     );
   });
 
-  afterEach(function() {
-      if (this.currentTest.state === "failed") {
-        cy.log(" failed" + this.currentTest.title);
-      } else {
-        cy.log("passed" + this.currentTest.title);
-      
+  afterEach(function () {
+    if (this.currentTest.state === "failed") {
+      cy.log("failed " + this.currentTest.title);
+    } else {
+      cy.log("passed " + this.currentTest.title);
     }
   });
+
   it("verify user can register successfully", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test@12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type(email);
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("japanese");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type(email);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("japanese");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Sign In");
   });
 
   it("verify user can register with empty Username", () => {
-    cy.get('[name="username"]');
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test@12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type(email);
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type(email);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Username is required");
   });
 
   it("verify user can register with mismatch password", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type(email);
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type(email);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "passsword do not match");
   });
 
   it("verify user can register with invalid email", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test@12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type(invalidemail);
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type(invalidemail123);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Invalid email");
   });
 
   it("verify user can register with empty password", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]');
-    cy.get('[name="repeatedPassword"]');
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type("aashika@gmail.com");
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field);
+    cy.get(selector.repeatpassword);
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type("aashika@gmail.com");
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "password is required.");
   });
 
   it("verify user can register with empty email", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test@12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]');
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Email is required");
   });
-    it("verify user can register with duplicate  email", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test@12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type("ashika@gmail.com");
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]').type("Kathmandu");
-    cy.get('[name="account.address2"]').type("Kathmandu");
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+
+  it("verify user can register with duplicate email", () => {
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type("ashika@gmail.com");
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1).type("Kathmandu");
+    cy.get(selector.address2).type("Kathmandu");
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Email already exixt");
   });
 
   it("verify user can register with empty address", () => {
-    cy.get('[name="username"]').type(username);
-    cy.get('[name="password"]').type("Test@12345");
-    cy.get('[name="repeatedPassword"]').type("Test12345");
-    cy.get('[name="account.firstName"]').type("Aashka");
-    cy.get('[name="account.lastName"]').type("Panta");
-    cy.get('[name="account.email"]').type(email);
-    cy.get('[name="account.phone"]').type("9812345678");
-    cy.get('[name="account.address1"]');
-    cy.get('[name="account.address2"]');
-    cy.get('[name="account.city"]').type("Kathmandu");
-    cy.get('[name="account.state"]').type("Bagmati");
-    cy.get('[name="account.zip"]').type("44600");
-    cy.get('[name="account.country"]').type("Nepal");
-    cy.get('[name="account.languagePreference"]').select("english");
-    cy.get('[name="account.favouriteCategoryId"]').select("DOGS");
-    cy.get('[name="newAccount"]').click();
+    cy.get(selector.username_field).type(username);
+    cy.get(selector.password_field).type("Test@12345");
+    cy.get(selector.repeatpassword).type("Test@12345");
+    cy.get(selector.firstname).type("Aashka");
+    cy.get(selector.lastname).type("Panta");
+    cy.get(selector.email).type(email);
+    cy.get(selector.phone).type("9812345678");
+    cy.get(selector.address1);
+    cy.get(selector.address2);
+    cy.get(selector.city).type("Kathmandu");
+    cy.get(selector.state).type("Bagmati");
+    cy.get(selector.zip).type("44600");
+    cy.get(selector.country).type("Nepal");
+    cy.get(selector.languages).select("english");
+    cy.get(selector.category).select("DOGS");
+    cy.get(selector.saveinformation).click();
+
     cy.get("body").should("contain.text", "Address is required");
   });
 });
---
+++ cypress/support/selector.js
@@ -0,0 +1,18 @@
+export const selector = {
+  username_field: '[name="username"]',
+  password_field: '[name="password"]',
+  repeatpassword: '[name="repeatedPassword"]',
+  firstname: '[name="account.firstName"]',
+  lastname: '[name="account.lastName"]',
+  email: '[name="account.email"]',
+  phone: '[name="account.phone"]',
+  address1: '[name="account.address1"]',
+  address2: '[name="account.address2"]',
+  city: '[name="account.city"]',
+  state: '[name="account.state"]',
+  zip: '[name="account.zip"]',
+  country: '[name="account.country"]',
+  languages: '[name="account.languagePreference"]',
+  category: '[name="account.favouriteCategoryId"]',
+  saveinformation: '[name="newAccount"]'
+};
